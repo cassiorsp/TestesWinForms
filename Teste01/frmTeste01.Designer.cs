f956b10256f712181fc8d23ee3c6116965fe7de6
CRSP <cassiorsp@gmail.com>
2023-06-05 19:20:06 -0300

    Adicionar arquivos de projeto.

---
+++ Teste01/frmTeste01.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Teste01
+{
+    partial class frmTeste01
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCalculadora = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrimeiroValor = new System.Windows.Forms.TextBox();
+            this.txtSegundoValor = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.tltMsg = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCalculadora
+            // 
+            this.lblCalculadora.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblCalculadora.AutoSize = true;
+            this.lblCalculadora.Font = new System.Drawing.Font("Calibri", 18F);
+            this.lblCalculadora.Location = new System.Drawing.Point(216, 9);
+            this.lblCalculadora.Name = "lblCalculadora";
+            this.lblCalculadora.Size = new System.Drawing.Size(131, 29);
+            this.lblCalculadora.TabIndex = 0;
+            this.lblCalculadora.Text = "Calculadora";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(12, 184);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(70, 30);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.CalcularAlgo);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(296, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14F);
+            this.label1.Location = new System.Drawing.Point(8, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Primeiro Valor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14F);
+            this.label2.Location = new System.Drawing.Point(8, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Segundo Valor";
+            // 
+            // txtPrimeiroValor
+            // 
+            this.txtPrimeiroValor.Location = new System.Drawing.Point(128, 54);
+            this.txtPrimeiroValor.Name = "txtPrimeiroValor";
+            this.txtPrimeiroValor.Size = new System.Drawing.Size(163, 27);
+            this.txtPrimeiroValor.TabIndex = 6;
+            this.txtPrimeiroValor.TextChanged += new System.EventHandler(this.HabilitaBotaoCalcular);
+            this.txtPrimeiroValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RegrasTxtBox);
+            // 
+            // txtSegundoValor
+            // 
+            this.txtSegundoValor.Location = new System.Drawing.Point(128, 98);
+            this.txtSegundoValor.Name = "txtSegundoValor";
+            this.txtSegundoValor.Size = new System.Drawing.Size(163, 27);
+            this.txtSegundoValor.TabIndex = 7;
+            this.txtSegundoValor.TextChanged += new System.EventHandler(this.HabilitaBotaoCalcular);
+            this.txtSegundoValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RegrasTxtBox);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Calibri", 14F);
+            this.lblResultado.ForeColor = System.Drawing.Color.Green;
+            this.lblResultado.Location = new System.Drawing.Point(8, 139);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(86, 23);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // tltMsg
+            // 
+            this.tltMsg.IsBalloon = true;
+            this.tltMsg.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // frmTeste01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtSegundoValor);
+            this.Controls.Add(this.txtPrimeiroValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblCalculadora);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmTeste01";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teste 01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCalculadora;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrimeiroValor;
+        private System.Windows.Forms.TextBox txtSegundoValor;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ToolTip tltMsg;
+    }
+}
+
---
+++ Teste01/frmTeste01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste01
+{
+    public partial class frmTeste01 : Form
+    {
+        public frmTeste01()
+        {
+            InitializeComponent();
+            //desativa o botão calcular
+            btnCalcular.Enabled = false;
+        }
+
+        private void CalcularAlgo(object sender, EventArgs e)
+        {
+            var pValor = long.Parse(txtPrimeiroValor.Text);
+            var sValor = long.Parse(txtSegundoValor.Text);
+            var rValor = pValor + sValor;
+
+            lblResultado.Text += "\n Hora do Resultado: " + 
+                DateTime.Now + " Calculo: " + pValor + "+" + 
+                sValor + "= " + rValor;
+        }
+
+        private void RegrasTxtBox(object sender, KeyPressEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+
+            if (!(char.IsNumber(e.KeyChar) && txt.TextLength < 5)
+                 && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+
+                tltMsg.ToolTipTitle = "Atenção!!!";
+                tltMsg.Show("Por favor digite apenas numeros.\nObrigado.", txt, 20, -80, 2000);
+            } 
+        }
+
+        private void HabilitaBotaoCalcular(object sender, EventArgs e)
+        {
+          if ( txtPrimeiroValor.TextLength > 0 && txtSegundoValor.TextLength > 0)
+                btnCalcular.Enabled = true;
+            else
+                btnCalcular.Enabled = false;
+        }
+    }
+}
