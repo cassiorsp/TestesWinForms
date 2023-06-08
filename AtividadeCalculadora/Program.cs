ca22822afd0fb888f6e8285f143877190dcc1d75
lucasambrosio12 <133067430+lucasambrosio12@users.noreply.github.com>
2023-06-07 21:15:07 -0300

    Entrega Lucas Ambrosio

--- AtividadeCalculadora/Program.cs
+++ AtividadeCalculadora/Program.cs
@@ -16,7 +16,7 @@ namespace AtividadeCalculadora
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LucasAmbrosio());
         }
     }
 }
